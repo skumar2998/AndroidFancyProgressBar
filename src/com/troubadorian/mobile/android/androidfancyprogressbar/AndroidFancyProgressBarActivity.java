3c27aa957f789e00fcad0e07a310b336f782706a
Das <fivemillivolts@gmail.com>
2011-08-19 18:52:57 -0400

    Initial commit

---
+++ src/com/troubadorian/mobile/android/androidfancyprogressbar/AndroidFancyProgressBarActivity.java
@@ -0,0 +1,14 @@
+package com.troubadorian.mobile.android.androidfancyprogressbar;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidFancyProgressBarActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

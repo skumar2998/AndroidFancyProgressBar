a86c1c60260e5b3623928292a9f8518a8d7fa59c
Das <fivemillivolts@gmail.com>
2011-08-22 19:20:06 -0400

    Added AsyncTask code

--- gen/com/troubadorian/mobile/android/androidfancyprogressbar/R.java
+++ gen/com/troubadorian/mobile/android/androidfancyprogressbar/R.java
@@ -12,10 +12,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int progress_bar_states=0x7f020001;
+        public static final int progress_bar_fill=0x7f020001;
+        public static final int progress_bar_shell=0x7f020002;
+        public static final int progress_bar_states=0x7f020003;
+        public static final int progressbar_animation=0x7f020004;
+        public static final int progressbar_blue_1=0x7f020005;
+        public static final int progressbar_blue_2=0x7f020006;
+        public static final int progressbar_blue_3=0x7f020007;
+        public static final int progressbar_blue_4=0x7f020008;
+        public static final int progressbar_blue_shell=0x7f020009;
+        public static final int progressbar_blue_small=0x7f02000a;
+        public static final int progressbar_shell=0x7f02000b;
+        public static final int progressbar_step1_1=0x7f02000c;
+        public static final int progressbar_step1_2=0x7f02000d;
+        public static final int progressbar_step1_3=0x7f02000e;
+        public static final int progressbar_step1_4=0x7f02000f;
+        public static final int progressbar_step2_1=0x7f020010;
+        public static final int progressbar_step2_2=0x7f020011;
+        public static final int progressbar_step2_3=0x7f020012;
+        public static final int progressbar_step2_4=0x7f020013;
     }
     public static final class id {
-        public static final int progressBar=0x7f050000;
+        public static final int cancel_button=0x7f050001;
+        public static final int custom_progressbar=0x7f050002;
+        public static final int percent_field=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/troubadorian/mobile/android/androidfancyprogressbar/AndroidFancyProgressBarActivity.java
+++ src/com/troubadorian/mobile/android/androidfancyprogressbar/AndroidFancyProgressBarActivity.java
@@ -1,14 +1,177 @@
 package com.troubadorian.mobile.android.androidfancyprogressbar;
 
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.graphics.Color;
+import android.graphics.drawable.*;
+import android.graphics.drawable.shapes.RoundRectShape;
 
-public class AndroidFancyProgressBarActivity extends Activity {
+public class AndroidFancyProgressBarActivity extends Activity
+{
+    private static final String TAG = "AndroidFancyProgressBarActivity";
+
+    ProgressDialog myProgress;
+
+    protected TextView percentField;
+
+    protected Button cancelButton;
+
+    protected InitTask initTask;
+
+    protected class InitTask extends AsyncTask<Context, Integer, Integer>
+    {
+        int myProgressCounter;
+
+        @Override
+        protected void onPostExecute(Integer result)
+        {
+
+            super.onPostExecute(result);
+            Log.i(TAG, "--------------------------------------------------onPostExecute(): " + result);
+            percentField.setText(result.toString());
+            percentField.setTextColor(0xFF69adea);
+            cancelButton.setVisibility(View.INVISIBLE);
+//            setContentView(R.layout.main); 
+
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            Log.d(TAG, "--------------------------------------------------onPreExecute() was called");
+
+            myProgressCounter = 0;
+        }
+
+        @Override
+        protected Integer doInBackground(Context... params)
+        {
+            /* start of copying files from assets to cache */
+            AssetManager assetManager = getAssets();
+            String[] files = null;
+            try
+            {
+                files = assetManager.list("cachefiles");
+                Log.d(TAG, "-----------how many files are in the folder" + files.length);
+            } catch (IOException e)
+            {
+                Log.e(TAG, "---------was there a problem" + e.getMessage());
+            }
+            for (int i = 0; i < files.length; i++)
+            {
+                InputStream in = null;
+                OutputStream out = null;
+                /*
+                 * String path = Environment.getExternalStorageDirectory()
+                 * .getAbsolutePath() + "/Android/data/" + "/files/";
+                 */
+
+                /* getFilesDir() will create the /files folder */
+                File filepath = getFilesDir();
+                Log.d(TAG, "---------------the file path is " + filepath);
+                try
+                {
+                    in = assetManager.open("cachefiles/" + files[i]);
+                    out = new FileOutputStream(filepath + "/" + files[i]);
+                    Log.d(TAG, "-------what's really happening here-----" + out.toString());
+                    copyFile(in, out);
+                    in.close();
+                    in = null;
+                    out.flush();
+                    out.close();
+                    out = null;
+                    publishProgress(i);
+                } catch (IOException ex)
+                {
+                    Log.e(TAG, "------------------------------bad things" + ex.getMessage());
+                }
+            }
+            /* end of copying files from assets to cache */
+            return 100;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values)
+        {
+            super.onProgressUpdate(values);
+            Log.i("makemachine", "onProgressUpdate(): " + String.valueOf(values[0]));
+            percentField.setText((values[0] * 2) + "%");
+            percentField.setTextSize(values[0]);
+
+            Log.d(TAG, "--------------------------------------------------onProgressUpdate() was called");
+        }
+
+        // -- called if the cancel button is pressed
+        @Override
+        protected void onCancelled()
+        {
+            super.onCancelled();
+            Log.i("makemachine", "onCancelled()");
+            percentField.setText("Cancelled!");
+            percentField.setTextColor(0xFFFF0000);
+        }
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1)
+        {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    protected class CancelButtonListener implements View.OnClickListener
+    {
+        public void onClick(View v)
+        {
+            initTask.cancel(true);
+        }
+    }
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        percentField = (TextView) findViewById(R.id.percent_field);
+        cancelButton = (Button) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new CancelButtonListener());
+        ProgressBar myProgressBar = (ProgressBar) findViewById(R.id.custom_progressbar);
+        // myProgressBar.setProgressDrawable(AndroidFancyProgressBarActivity.this.getResources().getDrawable(R.drawable.progress_bar_states));
+//        myProgressBar.setMax(100);
+//        myProgressBar.setProgress(50);
+        // final float[] roundedCorners = new float[] { 5, 5, 5, 5, 5, 5, 5, 5
+        // };
+        // ShapeDrawable pgDrawable = new ShapeDrawable(new
+        // RoundRectShape(roundedCorners, null,null));
+        // String MyColor = "#FF00FF";
+        // pgDrawable.getPaint().setColor(Color.parseColor(MyColor));
+        // ClipDrawable progress = new ClipDrawable(pgDrawable, Gravity.LEFT,
+        // ClipDrawable.HORIZONTAL);
+        // pg.setProgressDrawable(progress);
+        // pg.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.progress_horizontal));
+
+        initTask = new InitTask();
+        initTask.execute(this);
     }
 }

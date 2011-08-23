c61bcf988f0a8c5f11e23c88dbe6ca6eb62814b5
Das <fivemillivolts@gmail.com>
2011-08-23 19:38:07 -0400

    Added progressbar_blue_shell for background

--- gen/com/troubadorian/mobile/android/androidfancyprogressbar/R.java
+++ gen/com/troubadorian/mobile/android/androidfancyprogressbar/R.java
@@ -52,7 +52,8 @@ public final class R {
     }
     public static final class id {
         public static final int cancel_button=0x7f060001;
-        public static final int custom_progressbar=0x7f060002;
+        public static final int custom_progressbar=0x7f060003;
+        public static final int default_progressbar=0x7f060002;
         public static final int percent_field=0x7f060000;
     }
     public static final class layout {
